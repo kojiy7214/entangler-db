b135d15d8544a5f8ddf50e2c791d7b3dc6d8e4db
kojiy <koji7214@gmail.com>
2018-10-16 15:20:27 +0900

    Document serializing and event send/recv implemented

--- index.js
+++ index.js
@@ -1,338 +1,53 @@
-const path = require('path');
-const fs = require('fs');
-const Utils = require('simpleutils');
-const Map = Utils.Map;
-const EventAware = Utils.EventAware;
-const generateUuid = Utils.generateUuid;
-const mix = Utils.mix;
-const getClassFromName = Utils.getClassFromName;
+const Serializable = require("jsclass-serializer");
+const Map = require("jsclass-map");
+const EventAware = require("jsclass-event");
+const mix = require("jsclass-mixin");
 
+let documents = Symbol();
 
-/*
-
-*/
-
-/**
- * This is just a marker class, which should be used when a class needs
- * catch an event when document data is changed.
- */
-class DocumentDataAware {};
-
-/**
- * This is just a marker class, which should be used when a class needs
- * catch an event when document tags are changed.
- */
-class DocumentTagAware {};
-
-/**
- * Every query to SimpleNoSql is returned as document.
- * Document is a wrapper to application data, which adds
- * "id" and "collection" properties outside of it.
- */
-class Document extends EventAware {
-  /**
-   * Workaround to bind "this" to filter function.
-   */
-  static _dataAwareFilterFactory(srcid) {
-    return function(e) {
-      return (e instanceof DocumentDataAware || (e.isMixedWith && e.isMixedWith(DocumentDataAware))) || (e instanceof Document && srcid === e._id)
-    }
+class Entangler {
+  static setStoragePath(storage_path) {
+    Serializable.setStoragePath(storage_path);
   }
 
-  static _tagAwareFilterFactory(srcid) {
-    return function(e) {
-      return (e instanceof DocumentTagAware || (e.isMixedWith && e.isMixedWith(DocumentTagAware))) || (e instanceof Document && srcid === e._id)
-    }
+  constructor() {
+    this[documents] = new Map();
   }
 
-  static _bindDataToProperty(target, data) {
-    Object.keys(data).forEach(function(prop) {
-      !target[prop] && Object.defineProperty(target, prop, {
-        get: function() {
-          return data[prop];
-        },
-        set: function(val) {
-          data[prop] = val;
-          this.dispatchTo(Document._dataAwareFilterFactory(this._id), "propertyChanged", this, prop, val);
-        }
-      });
-    });
-  }
-
-
-  constructor(id, tags, data) {
-    super();
-
-    this._id = id;
-    if (Array.isArray(tags)) {
-      this._tags = new Set(tags);
-    } else if (tags) {
-      throw "tags should be an Array";
-    } else {
-      this._tags = new Set();
-    }
-    this._data = data;
-
-    Document._bindDataToProperty(this, this._data);
-    this.dispatchTo(Document._dataAwareFilterFactory(this._id), "dataChanged", this);
-  }
-
-  getId() {
-    return this._id;
-  }
-
-  getTags() {
-    return Array.from(this._tags);
-  }
-
-  addTags(tags) {
-    if (Array.isArray(tags)) {
-      tags.forEach(o => this._tags.add(o));
-    } else {
-      this._tags.add(tags);
-    }
-    this.dispatchTo(Document._tagAwareFilterFactory(this._id), "tagsAdded", this.getId(), tags);
-  }
-
-  removeTags(tags) {
-    if (Array.isArray(tags)) {
-      tags.forEach(o => this._tags.delete(o));
-    } else {
-      this._tags.delete(tag);
-    }
-    this.dispatchTo(Document._tagAwareFilterFactory(this._id), "tagsRemoved", this.getId(), tags);
-  }
-
-  setData(data) {
-    Object.assign(this._data, data);
-    Document._bindDataToProperty(this, this._data);
-    this.dispatchTo(Document._dataAwareFilterFactory(this._id), "dataChanged", this);
-  }
-
-  getData() {
-    return this._data;
-  }
-
-  onPropertyChanged(doc, key, val) {
-    this._data[key] = val;
-    Document._bindDataToProperty(this, this._data);
-  }
-
-  onDataChanged(doc) {
-    Object.assign(this._data, doc._data);
-    Document._bindDataToProperty(this, this._data);
-  }
-
-  onTagsAdded(id, tags) {
-    if (Array.isArray(tags)) {
-      tags.forEach(o => this._tags.add(o));
-      this.dispatchTo()
-    } else {
-      this._tags.add(tags);
-    }
-  }
-
-  onTagsRemoved(id, tags) {
-    this.removeTags(tags);
-    if (Array.isArray(tags)) {
-      tags.forEach(o => this._tags.delete(o));
-    } else {
-      this._tags.delete(tag);
-    }
+  addDocument(doc) {
+    this[documents].set(doc.uuid, doc);
   }
 }
 
-/**
- * In-Memory NoSql DB alike.
- * !!BE AWARE!! This DB alike class is NOT FOR PRODUCTION.
- * You are advised NOT TO USE SIMPLENOSQL in you productoin.
- */
-class SimpleNoSql extends mix(EventAware, DocumentDataAware, DocumentTagAware) {
-  static instance(dbpath) {
-    return global.simple_no_sql || new SimpleNoSql(dbpath);
-  }
-
-  constructor(dbpath) {
+class Document extends mix(Serializable, EventAware) {
+  constructor() {
     super();
+    EventAware.new(this);
 
-    this._dbpath = dbpath;
+    entangler_db.addDocument(this);
 
-    // primary index map
-    this._idx = new Map();
-    //virtual index map, used mainly for tag support
-    this._vidx = new Map();
-
-    if (!global.simple_no_sql) {
-      global.simple_no_sql;
-    }
-  }
-
-  _getTagMap(tag) {
-    let retval = this._vidx.get(tag);
-
-    if (!retval) {
-      retval = new Map();
-      this._vidx.set(tag, retval);
-    }
-    return retval;
-  }
-
-  _addToVirtualIndexMap(id, doc) {
-    let tags = doc.getTags();
-    for (let o in tags) {
-      let tagmap = this._getTagMap(tags[o]);
-      tagmap.set(id, doc);
+    let handler = {
+      set: function(obj, prop, value) {
+        if (obj[prop] !== value) {
+          obj[prop] = value;
+          let data = {};
+          data.key = prop;
+          data.val = value;
+          obj.dispatchTo(o => o.uuid && o.uuid === obj.uuid, "stateChanged", data);
+        }
+        return true;
+      }
     };
+
+    return new Proxy(this, handler);
   }
 
-  onTagAdded(id, tags) {
-    if (Array.isArray(tags)) {
-      tags.forEach(
-        function(o) {
-          let tagmap = this._getTagMap(o);
-          tagMap.set(id, this._idx.get(id));
-        }
-      );
-    } else {
-      let tagmap = this._getTagMap(tag);
-      tagMap.set(id, this._idx.get(id));
-    }
-  }
-
-  onTagRemoved(id, tags) {
-    if (Array.isArray(tags)) {
-      tags.forEach(
-        function(o) {
-          let tagmap = this._getTagMap(o);
-          tagMap.delete(id);
-        }
-      );
-    } else {
-      let tagmap = this._getTagMap(tag);
-      tagMap.delete(id);
-    }
-  }
-
-  onDataChanged(doc) {
-    console.log("SIMPLENOSQL HAS CATCHED THE DATA CHANGE EVENT.");
-  }
-
-  onPropertyChanged(doc, prop, va) {
-    console.log("SIMPLENOSQL HAS CATCHED THE PROPERTY CHANGE EVENT.");
-  }
-
-  create(data, tags) {
-    //get unique id
-    let id = undefined;
-    while (!id) {
-      id = generateUuid();
-      if (this._idx.get(id)) {
-        id = undefined;
-      }
-    }
-
-    let doc = new Document(id, tags, data);
-
-    //set to idx map for fast get
-    this._idx.set(id, doc);
-    //add to virtual idx map for faset find
-    this._addToVirtualIndexMap(id, doc);
-    return doc;
-  }
-
-  get(id) {
-    if (id instanceof Document) {
-      id = id.getId();
-    }
-    return this._idx.get(id);
-  }
-
-  delete(id) {
-    if (id instanceof Document) {
-      id = id.getId();
-    }
-    this._idx.delete(id);
-  }
-
-  findAnd(data, tags) {
-    let retval = new SimpleNoSql();
-    let idxmap = this._idx;
-
-    if (tags) {
-      idxmap = this._getTagMap(tags);
-      if (!idxmap) {
-        return retval;
-      }
-    }
-
-    idxmap.keys().forEach(function(o) {
-      let target = idxmap.get(o);
-      let match = true;
-      for (let e in target) {
-        for (let e in data) {
-          if (!target[e] || target[e] !== data[e]) {
-            match = false;
-            break;
-          }
-        }
-        if (!match) {
-          break;
-        }
-      }
-      if (match) {
-        retval._idx.set(o, target);
-        retval._addToVirtualIndexMap(o, target);
-      }
-    })
-    return retval;
-  }
-
-  findOr(data, tags) {
-    let retval = new SimpleNoSql();
-    let idxmap = this._idx;
-
-    if (tags) {
-      idxmap = this._getTagMap(tags);
-      if (!idxmap) {
-        return retval;
-      }
-    }
-
-    idxmap.keys().forEach(function(o) {
-      let target = idxmap.get(o);
-      let match = false;
-      for (let e in target) {
-        for (let e in data) {
-          if (target[e] && target[e] === data[e]) {
-            match = true;
-            break;
-          }
-        }
-        if (!match) {
-          break;
-        }
-      }
-      if (match) {
-        retval._idx.set(o, target);
-        retval._addToVirtualIndexMap(o, target);
-      }
-    })
-
-    return retval;
-  }
-
-  getDocumentArray() {
-    let retval = [];
-    let idxmap = this._idx;
-
-    idxmap.keys().forEach(function(o) {
-      retval.push(idxmap.get(o));
-    });
-
-    return retval;
+  onStateChanged(data) {
+    this[data.key] = data.val;
   }
 }
 
-module.exports.SimpleNoSql = SimpleNoSql;
+var entangler_db = entangler_db || new Entangler();
+
+module.exports.entangler_db = entangler_db;
 module.exports.Document = Document;
-module.exports.DocumentTagAware = DocumentTagAware;
--- test/test.js
+++ test/test.js
@@ -1,204 +1,41 @@
-let assert = require('assert');
+const Document = require("../../entangler-db").Document;
+const entangler_db = require("../../entangler-db").entangler_db;
+const Serializable = require("jsclass-serializer");
+const assert = require("assert");
+const EventAware = require("jsclass-event");
 
-const SimpleNoSql = require('../../entangler-db').SimpleNoSql;
-const Document = require('../../entangler-db').Document;
-const DocumentTagAware = require('../../entangler-db').DocumentTagAware;
+Serializable.setStoragePath("./data/");
 
-// TEST SET FOR DOCUMENT
-let doc1;
-let doc2;
+describe('Document', function() {
+  describe('Create New Subclass', function() {
+    let a1 = undefined;
+    class A extends Document {
+      constructor() {
+        super();
+      }
+    };
 
-describe('TESTING ### Document ###', function() {
-  describe('create Document instance', function() {
-    it('#construtor()', function() {
-      let id = "test";
-      let tags = ["test_tag1"];
-      let data = {
-        a: "A",
-        b: "B"
-      };
-      doc1 = new Document(id, tags, data);
+    it('#constructor()', function() {
+      a1 = new A();
     })
-  })
 
-  describe('get data from Document', function() {
-    it('#get()', function() {
-      assert.equal(doc1.a, "A");
-      assert.equal(doc1.b, "B");
-      assert.equal(doc1.c, undefined);
+    it('#saveToFile', function() {
+      a1.saveToFile();
     })
-  })
 
-  describe('sync data between Documents on creation', function() {
-    it('#constructor() -> dispatchTo()', function() {
-      let id = "test";
-      let tags = ["test_tag1"];
-      let data = {
-        a: "1",
-        b: "2",
-        c: "C"
-      };
-      doc2 = new Document(id, tags, data);
+    it('#loadFromFile', function() {
+      let a2 = Serializable.loadFromFile(a1.uuid);
 
-      assert.equal(doc1.a, "1");
-      assert.equal(doc1.b, "2");
-      assert.equal(doc1.c, "C");
+      assert.equal(a2 instanceof A, true);
+      assert.equal(a2 instanceof Serializable, true);
+      assert.equal(a2 instanceof EventAware, true);
 
-    })
-  })
+      assert.equal(a2.a, undefined);
+      a1.a = "a";
+      assert.equal(a2.a, "a");
 
-  describe('sync data between Documents on setProperty', function() {
-    it('#property accessor -> dispatchTo()', function() {
-      doc1.c = "E";
-      assert.equal(doc2.c, "E");
-    })
-  })
-
-  describe('sync data between Documents on setData', function() {
-    it('#setData -> dispatchTo()', function() {
-      let data = {
-        a: "1",
-        c: "2",
-        e: "3",
-        g: "4"
-      };;
-      doc1.setData(data);
-
-      assert.equal(doc2.a, "1");
-      assert.equal(doc2.b, "2");
-      assert.equal(doc2.c, "2");
-      assert.equal(doc2.d, undefined);
-      assert.equal(doc2.e, "3");
-      assert.equal(doc2.f, undefined);
-      assert.equal(doc2.g, "4");
-
-      assert.equal(doc1.a, "1");
-      assert.equal(doc1.b, "2");
-      assert.equal(doc1.c, "2");
-      assert.equal(doc1.d, undefined);
-      assert.equal(doc1.e, "3");
-      assert.equal(doc1.f, undefined);
-      assert.equal(doc1.g, "4");
-    })
-  })
-})
-
-
-// TEST SET FOR SimpleNoSql
-let simple_no_sql = undefined;
-describe('TESTING ### SimpleNoSql ###', function() {
-  describe('create SimpleNoSql instance', function() {
-    it('#construtor()', function() {
-      simple_no_sql = new SimpleNoSql();
-    })
-  })
-
-  let collectionname = "test";
-
-  describe('insert data', function() {
-    it('#construtor()', function() {
-      let data = {
-        "a": "A",
-        "b": "B"
-      };
-
-      id = simple_no_sql.create(data, ["test_tag_1"]);
-    })
-  })
-
-  describe('get data with id', function() {
-    it('#get()', function() {
-      let data = {
-        "a": "A",
-        "b": "B"
-      };
-
-      id = simple_no_sql.create(data, ["test_tag_1"]);
-      data = simple_no_sql.get(id);
-      assert.equal(data.a, "A");
-      assert.equal(data.b, "B");
-
-      simple_no_sql.create(data);
-
-    })
-  })
-
-  describe('delete data with id', function() {
-    it('#delete()', function() {
-      let data = {
-        "a": "A",
-        "b": "B"
-      };
-
-      id = simple_no_sql.create(data);
-      data = simple_no_sql.get(id);
-
-      data.a = "A";
-
-      assert.equal(data.a, "A");
-      assert.equal(data.b, "B");
-      simple_no_sql.delete(id);
-      data = simple_no_sql.get(id);
-      assert.equal(data, undefined);
-    })
-  })
-
-  describe('find using [AND] operator', function() {
-    it('#findAnd()', function() {
-      let data = {
-        "a": "P",
-        "b": "Q"
-      };
-      id = simple_no_sql.create(data);
-
-      let q1 = {
-        "a": "P"
-      };
-      let ids = simple_no_sql.findAnd(q1).getDocumentArray();
-      assert.equal(ids.length, 1);
-      assert.equal(ids[0].getId(), id.getId());
-
-      q1 = {
-        "a": "P",
-        "b": "Q"
-      };
-      ids = simple_no_sql.findAnd(q1).getDocumentArray();
-      assert.equal(ids.length, 1);
-      assert.equal(ids[0].getId(), id.getId());
-
-      q1 = {
-        "a": "A",
-        "b": "B"
-      };
-      ids = simple_no_sql.findAnd(q1).getDocumentArray();
-      assert.equal(ids.length, 2);
-
-      q1 = {
-        "a": "A",
-        "b": "Q"
-      };
-      ids = simple_no_sql.findAnd(q1).getDocumentArray();
-      assert.equal(ids.length, 0);
-    })
-  })
-
-  describe('find using [OR] operator', function() {
-    it('#findOr()', function() {
-      let q1 = {
-        "a": "A",
-        "b": "Q"
-      };
-
-      let ids = simple_no_sql.findOr(q1, "test_tag_1").getDocumentArray();
-      assert.equal(ids.length, 2);
-    })
-  })
-
-  describe('add Tag', function() {
-    it('#addTag()', function() {
-      doc1.addTags("test22");
-      assert.equal(doc1.getTags().includes("test22"), true);
-      assert.equal(doc2.getTags().includes("test22"), true);
+      a2.a = "b";
+      assert.equal(a1.a, "b");
     })
   })
 })
